f4f11f808a8f10394284814183ff813c966c44ba
Alexandre Lermant <alermant.dev@outlook.fr>
2019-01-23 13:20:42 +0100

    recherche prochain match

--- serverhome/plugins/football/controller.js
+++ serverhome/plugins/football/controller.js
@@ -14,31 +14,35 @@ class FootballController {
         res.end(JSON.stringify(dataView));
     }
 
-	postAction(req, res){
+	async postAction(req, res){
 		var now = new Date();
 		let result = {
 			resultText: "Je n'ai pas trouvé de résultat",
 			data: []
 		}
-		console.log('functions', functions);
-		switch (req.params.actionId) {
-			case "next-match-competition":
-				// donne-moi le prochain match de la (?P<competition>.+)
-				result = functions.getNextMatch(req.body.competition);
-			break;
-			case "give-me-every-match-from":
-				// donne-moi tous les matchs de la (?P<competition>.+)
-				result = functions.getEveryMatch(req.body.competition);
-			break;
-			case "give-me-result":
-				// donne-moi le résultat (?P<equipe1>.+) contre (?P<equipe2>.+) de la (?P<competition>.+)
-				result = functions.getResult(req.body.equipe1, req.body.equipe2, req.body.competition);
-			break;
-			case "give-me-result-date":
-				// donne-moi le résultat de la (?P<competition>.+) du (?P<date>.+)
-				result = functions.giveMeResultDate(req.body.competition, req.body.date);
-			break;
+		try {
+			switch (req.params.actionId) {
+				case "next-match-competition":
+					// donne-moi le prochain match de la (?P<competition>.+)
+					result = await functions.getNextMatch(req.body.competition);
+				break;
+				case "give-me-every-match-from":
+					// donne-moi tous les matchs de la (?P<competition>.+)
+					result = await functions.getEveryMatch(req.body.competition);
+				break;
+				case "give-me-result":
+					// donne-moi le résultat (?P<equipe1>.+) contre (?P<equipe2>.+) de la (?P<competition>.+)
+					result = await functions.getResult(req.body.equipe1, req.body.equipe2, req.body.competition);
+				break;
+				case "give-me-result-date":
+					// donne-moi le résultat de la (?P<competition>.+) du (?P<date>.+)
+					result = await functions.giveMeResultDate(req.body.competition, req.body.date);
+				break;
+			}
+		} catch (e) {
+			result.resultText = e.message;
 		}
+
 		res.json(result);
 	}
 }
--- serverhome/plugins/football/functions.js
+++ serverhome/plugins/football/functions.js
@@ -1,26 +1,52 @@
-function getNextMatch(competitionName) {
+const Axios = require('axios');
+Axios.defaults.baseURL = 'http://api.football-data.org/v2/'
+Axios.defaults.headers.common['X-Auth-Token'] = process.env.API_KEY_FOOTBALL
 
+const getCompetition = 'competitions?plan=TIER_ONE';
+
+async function getCompetitionId(competitionName) {
+    const {data} = await Axios.get(`competitions?plan=TIER_ONE`);
+    const competitionId = data.competitions
+        .find(competition => competition.name.toUpperCase() === competitionName.toUpperCase());
+    if (!competitionId) {
+        throw `Pas de résultat pour ${competitionName}`;
+    }
+    return competitionId.id
+}
+
+async function getNextMatch(competitionName) {
+    const competitionId = await getCompetitionId(competitionName);
+    const {data} = await Axios.get(`competitions/${competitionId}/matches?status=SCHEDULED`);
+    const matche = data.matches[0];
+    if (!matche) {
+        throw 'Pas de futur match prévu.';
+    }
+    const date = new Date(matche.utcDate);
+
+    return {
+        resultText: `Le prochain match est prévu le
+            ${date.getDate()} ${date.getMonth()+1} ${date.getFullYear()}
+            à ${date.getHours()} ${date.getHours()} ${date.getMinutes() > 0 ? date.getMinutes() : ''} ${date.getFullYear()},
+            ${matche.homeTeam.name} contre ${matche.awayTeam.name}`,
+        data: matche
+    }
+}
+
+async function getEveryMatch(competitionName) {
+    return {
+        resultText: "Voici le résultat",
+        data: []
+    }
+}
+
+async function getResult(team1, team2, competitionName) {
     return {
         resultText: "Je n'ai pas trouvé de résultat",
         data: []
     }
 }
 
-function getEveryMatch(competitionName) {
-    return {
-        resultText: "Je n'ai pas trouvé de résultat",
-        data: []
-    }
-}
-
-function getResult(team1, team2, competitionName) {
-    return {
-        resultText: "Je n'ai pas trouvé de résultat",
-        data: []
-    }
-}
-
-function giveMeResultDate(competition, date) {
+async function giveMeResultDate(competition, date) {
     return {
         resultText: "Je n'ai pas trouvé de résultat",
         data: []
